a0e3aea23e4036b9f22e9786bf9edbaeec9097ee
Rafał Michaluk <rm360179@students.mimuw.edu.pl>
2021-10-05 20:11:26 +0200

    init

---
+++ datasets.py
@@ -0,0 +1,149 @@
+from sklearn import datasets
+import numpy as np
+import sklearn
+
+
+def moons_dataset(bs):
+    x, y = datasets.make_moons(bs)
+    return x
+
+def moons_dataset_in_4d(bs):
+    x = moons_dataset(bs)
+    x_2 = np.zeros_like(x)
+    x = np.concatenate([x, x_2], axis=1)
+    return x
+
+def moons_2d_dataset_in_4d(bs):
+    x = moons_dataset(bs)
+    x = x + np.random.randn(*x.shape) * 0.05
+    x_2 = np.zeros_like(x)
+    x = np.concatenate([x, x_2], axis=1)
+    return x
+
+def line_dataset(bs):
+    x = np.random.randn(bs,2) / 2
+    x[:, 1] = x[:, 0]
+    return x
+
+def parabola_dataset(bs, seed=4):
+    x = np.random.randn(bs, 2) / 2
+    x[:, 1] = x[:, 0] ** 2
+    return x
+
+def parabola_3d_dataset(bs, seed=5):
+    x = np.random.randn(bs, 3) / 2
+    x[:, 1] = x[:, 0] ** 2
+    x[:, 2] = 1.0
+    return x
+
+def parabola_2d_dataset_in_4d(bs, seed=6):
+    x = np.random.randn(bs, 2)
+    x[:, 1] = x[:, 0] ** 2
+    x = x + np.random.randn(*x.shape) * 0.5
+    x_2 = np.ones_like(x)
+    x = np.concatenate([x, x_2], axis=1)
+    return x
+
+def parabola_2d_dataset_in_10d(bs):
+    x = np.random.randn(bs, 2)
+    x[:, 1] = x[:, 0] ** 2
+    x = x + np.random.randn(*x.shape) * 0.5
+    x_2 = np.zeros_like(x)
+    x = np.concatenate([x, x_2, x_2, x_2, x_2], axis=1)
+    return x
+
+def parabola_6d_dataset_in_18d(bs):
+    x = np.random.randn(bs, 6)
+    x[:, 1] = x[:, 0] ** 2
+    x[:, 2] = x[:, 0] ** 2
+    x[:, 3] = np.abs(x[:, 0]) ** 0.5
+    x[:, 4] = x[:, 0]
+    x = x + np.random.randn(*x.shape) * 0.5
+    x_2 = np.zeros_like(x)
+    x = np.concatenate([x, x_2, x_2], axis=1)
+    return x
+
+def s_dataset_in_6d(bs):
+    x = datasets.make_s_curve(bs)[0]
+    x_2 = np.zeros_like(x)
+    x = np.concatenate([x, x_2], axis=1)
+    return x
+
+def spirals_dataset(bs):
+    n = np.sqrt(np.random.rand(bs // 2, 1)) * 540 * (2 * np.pi) / 360
+    d1x = -np.cos(n) * n
+    d1y = np.sin(n) * n
+    x = np.vstack((np.hstack((d1x, d1y)), np.hstack((-d1x, -d1y)))) / 3
+    return x
+
+def lollipop_dataset(bs):
+    cs = int(0.95 * bs)
+    r = np.random.uniform(size=cs)
+    fi = np.random.uniform(0, 2 * np.pi, size=cs)
+    x = np.zeros((bs, 2))
+    x[:cs, 0] = r**0.5 * np.sin(fi)
+    x[:cs, 1] = r**0.5 * np.cos(fi)
+    x += 2
+    stick = np.random.uniform(high=2-1/np.sqrt(2), size=(bs-cs))
+    x[cs:, 0] = stick
+    x[cs:, 1] = stick
+    return x
+
+def uniform_helix_r3(bs):
+    x = np.zeros((bs, 3))
+    t = np.random.uniform(-1, 1, size=bs)
+    x[:, 0] = np.sin(np.pi*t)
+    x[:, 1] = np.cos(np.pi*t)
+    x[:, 2] = t
+    return x
+
+
+def swiss_roll_r3(bs):
+    x = sklearn.datasets.make_swiss_roll(bs)[0]
+    x -= x.mean(axis=0).reshape(1,3)
+    x /= x.std(axis=0).reshape(1,3)
+    return x
+
+def N_5(bs):
+    return np.random.normal(size=(bs,5))
+
+def uniform_sphere_S7(bs):
+    pass
+
+def uniform_12(bs):
+    return np.random.uniform(low=-0.5, high=0.5, size=(bs,12))
+
+def sphere_7(bs):
+    x = np.random.normal(size=(bs,8))
+    lam = np.sqrt(np.sum(x**2, axis=1, keepdims=True))
+    x = x/lam
+    return x
+
+def N_1_2(bs):
+    x = np.random.normal(size=(bs,1))
+    x = np.concatenate([x, x], axis=1)
+    return x
+
+def N_10_20(bs):
+    x = np.random.normal(size=(bs,10))
+    x = np.concatenate([x, x], axis=1)
+    return x
+
+def N_100_200(bs):
+    x = np.random.normal(size=(bs,100))
+    x = np.concatenate([x, x], axis=1)
+    return x
+
+def N_1000_2000(bs):
+    x = np.random.normal(size=(bs,1000))
+    x = np.concatenate([x, x], axis=1)
+    return x
+
+def N_10000_20000(bs):
+    x = np.random.normal(size=(bs,10000))
+    x = np.concatenate([x, x], axis=1)
+    return x
+
+def generate(generator, bs, seed):
+    np.random.seed(seed)
+    return generator(bs)
---
+++ dim_estimators.py
@@ -0,0 +1,116 @@
+import os
+import sys
+import torch
+from torch.utils.data import DataLoader
+sys.path.append(os.path.abspath('./dimensions'))
+from estimators.mle import update_nn, KNNComputerNoCheck, intrinsic_dim_sample_wise_double_mle
+from torch.utils.data import Dataset
+from scipy.spatial import distance_matrix
+import numpy as np
+from sklearn import linear_model
+from likelihood_estimators import LLGaussianMixtures, LLFlow, LLGlow
+import pickle
+
+
+def mle(data, k, batch_size=1024, device='cuda'):
+    data_loader = torch.utils.data.DataLoader(list(zip(data, [0] * data.shape[0])))
+    print("Computing the KNNs")
+    nn_computer = KNNComputerNoCheck(data.shape[0], K=k + 1).to(device)
+    update_nn(data_loader, 0, data_loader, 0, nn_computer, device)
+    dist = nn_computer.min_dists.cpu().numpy()
+    _, invmle = intrinsic_dim_sample_wise_double_mle(k, dist)
+
+    return invmle
+
+
+def corr_dim_per_sample(data, l_perc=0.000001, u_perc=0.01):
+    N = data.shape[0]
+    distances = distance_matrix(data, data,p=2)
+    r_low, r_high = np.quantile(distances[np.triu_indices(N, k=1)], [l_perc, u_perc])
+
+    rs_samples = []
+    r_list = np.linspace(r_low, r_high, 10) 
+
+    for r in r_list:
+        distances_r = distances <= r
+        print(f'persample, r = {r}, percenttrue: {(distances_r.sum())/distances_r.size}')
+        likelihoods = (distances_r.sum(axis=1) - 1)/(N-1)
+        rs_samples.append(likelihoods)
+
+    samples_rs = np.transpose(np.array(rs_samples))
+    dims_per_sample = list()
+    for i in range(samples_rs.shape[0]):
+        sample_likelihoods = samples_rs[i]
+        regr = linear_model.LinearRegression()
+        regr.fit(np.log10(r_list).reshape(-1, 1), np.log10(sample_likelihoods))
+        dims_per_sample.append(regr.coef_[0])
+
+    return dims_per_sample
+
+
+def corr_dim(data, l_perc=0.000001, u_perc=0.01):
+    N = len(data)
+    distances = distance_matrix(data, data,p=2)[np.triu_indices(N, k=1)]
+    r_low, r_high = np.quantile(distances, [l_perc, u_perc])
+
+    C_r_list = []
+    r_list = np.linspace(r_low, r_high, 3)
+
+    for r in r_list:
+        distances_r = distances <= r
+        print(f'total, r = {r}, percenttrue: {(distances_r.sum())/distances_r.size}')
+        C_r = 2 * distances_r.sum()/N/(N-1)
+        C_r_list.append(C_r)
+
+    regr = linear_model.LinearRegression()
+    regr.fit(np.log10(r_list).reshape(-1,1), np.log10(C_r_list))
+    return regr.coef_[0]
+
+
+class LIDL():
+    def __init__(self, model_type):
+        if model_type == 'gaussian_mixture':
+            self.model = LLGaussianMixtures()
+        elif model_type == 'rqnsf':
+            self.model = LLFlow('rqnsf')
+        elif model_type == 'maf':
+            self.model = LLFlow('maf')
+        elif model_type == 'glow':
+            self.model = LLGlow()
+        else:
+            assert False, 'incorrect model type'
+
+    def run_on_deltas(self, deltas, **model_args):
+        for delta in deltas:
+            self.model.run(delta=delta, **model_args)
+
+    def dims_on_deltas(self, deltas, epoch, total_dim):
+        indsort = np.argsort(np.array(deltas))
+        lls = np.array([self.model.results[delta][epoch] for delta in deltas])
+        lls = lls[indsort]
+        deltas = np.array(deltas)[indsort]
+
+
+        lls = lls.transpose()
+        dims = list()
+        for i in range(lls.shape[0]):
+            good_inds = ~np.logical_or(np.isnan(lls[i]), np.isinf(lls[i]))
+            ds = np.log(deltas[good_inds])
+            ll = lls[i][good_inds]
+            if ll.size < 2:
+                dims.append(np.nan)
+            else:
+                regr = linear_model.LinearRegression()
+                regr.fit(ds.reshape(-1, 1), ll)
+                regr.predict(ds.reshape(-1, 1))
+                dims.append(total_dim - regr.coef_[0])
+
+        return np.array(dims)
+
+    def save(self, name):
+        with open(f'{name}_lidl.obj', 'wb') as f:
+            pickle.dump(self.__dict__, f)
+
+    def load(self, name):
+        with open(f'{name}_lidl.obj', 'rb') as f:
+            self.__dict__ = pickle.load(f)
