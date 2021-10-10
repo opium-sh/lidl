d8075b2c2b247d2ab442ba188522571bfdea3bd8
Rafał Michaluk <rm360179@students.mimuw.edu.pl>
2021-10-10 04:30:12 +0200

    add scripts to run maf

---
+++ experiments/comparison/run_cpu.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 params_file seed"
+    exit 1;
+fi
+
+params=$1
+seed=$2
+while IFS= read -r line; do
+    python ../../run_experiments.py $line --seed=$seed &
+done < $params
---
+++ experiments/comparison/run_gpu.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 params_file seed"
+    exit 1;
+fi
+
+params=$1
+seed=$2
+while IFS= read -r line; do
+    srun --time 3-0 --qos=32gpu7d --gres=gpu python ../../run_experiments.py $line --seed=$seed &
+done < $params
