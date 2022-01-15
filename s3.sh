13231ac6475f27f35d8af54e8fa9788b10995f25
Rafał Michaluk <rm360179@students.mimuw.edu.pl>
2022-01-15 11:19:31 +0100

    new run

--- s3.sh
+++ s3.sh
@@ -19,18 +19,32 @@ algorithms=("skdim_corrint"
         "skdim_mle"
         "skdim_mom"
         "skdim_tle"
-        "skdim_twonn")
-dataset="swiss-roll-r3"
-for seed in {1..10}; do
+        "skdim_twonn"
+	"maf"
+	"rqnsf")
+datasets=("uniform_N_0_1-12"
+          "gaussian-5"
+	  "sphere-7"
+	  "uniform-helix-r3"
+	  "swiss-roll-r3"
+	  "gaussian-10-20"
+	  "gaussian-100-200")
+
+for seed in {1..5}; do
         for algorithm in "${algorithms[@]}"; do
-		srun --time 3-0 --qos=32gpu7d  --gres=gpu:1 python run_experiments.py \
-			--dataset $dataset \
-			--algorithm $algorithm \
-			--seed $seed \
-			--neptune_name rm360179/lidl-loss-mse \
-			--neptune_token "eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiI2MmVjN2EzYS04Y2FmLTRkYjItOTkyMi1mNmEwYWQzM2I3Y2UifQ==" \
-			--device cuda \
-			--ground_truth_const 2 &
+		for dataset in "${datasets[@]}"; do
+			echo $seed-$algorithm-$dataset
+			srun --time 3-0 --qos=32gpu7d  --gres=gpu:1 python run_experiments.py \
+				--dataset $dataset \
+				--algorithm $algorithm \
+				--seed $seed \
+			        --size 10000 \
+			        --delta 0.05 \
+		        	--num_deltas 11 \
+				--neptune_name rm360179/lidl-loss-mse \
+				--neptune_token "eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiI2MmVjN2EzYS04Y2FmLTRkYjItOTkyMi1mNmEwYWQzM2I3Y2UifQ==" \
+				--device cuda &
+			done;
                 done;
 	done;
 
